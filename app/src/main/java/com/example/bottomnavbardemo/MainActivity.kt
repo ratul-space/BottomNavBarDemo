1f7ebb7828a81a50c9856d6f3be093f8e2ee58ec
ratul-space <to.ratul.hosen@gmail.com>
2023-12-30 14:47:29 +0600

    Initial commit

---
+++ app/src/main/java/com/example/bottomnavbardemo/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.bottomnavbardemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bottomnavbardemo.ui.theme.BottomNavBarDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BottomNavBarDemoTheme {
+
+            }
+        }
+    }
+}
+
