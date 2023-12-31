0dc7b30df70b4c9cb1c4b4a8ce690c26c54eae8b
ratul-space <to.ratul.hosen@gmail.com>
2023-12-31 22:36:49 +0600

    refactor: move BottomBarScreen under navigation package

--- app/src/main/java/com/example/bottomnavbardemo/navigation/BottomBarScreen.kt
+++ app/src/main/java/com/example/bottomnavbardemo/navigation/BottomBarScreen.kt
@@ -1,4 +1,4 @@
-package com.example.bottomnavbardemo
+package com.example.bottomnavbardemo.navigation
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
