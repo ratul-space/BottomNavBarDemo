80ddc9cc616d76624aead46f3000a5d06a2f5cee
ratul-space <to.ratul.hosen@gmail.com>
2023-12-31 23:15:12 +0600

    feat(navigation): add NavHost under BottomNavGraph composable function

--- app/src/main/java/com/example/bottomnavbardemo/navigation/BottomNavGraph.kt
+++ app/src/main/java/com/example/bottomnavbardemo/navigation/BottomNavGraph.kt
@@ -2,8 +2,16 @@ package com.example.bottomnavbardemo.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 
 @Composable
 fun BottomNavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = BottomBarScreen.Home.route
+    ) {
+
+
+    }
 
 }
