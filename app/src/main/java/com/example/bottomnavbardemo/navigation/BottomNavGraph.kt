a8f1a48283cbf94df7c847f10d2f59521521c79d
ratul-space <to.ratul.hosen@gmail.com>
2023-12-31 23:10:11 +0600

    feat(navigation): add BottomNavGraph

---
+++ app/src/main/java/com/example/bottomnavbardemo/navigation/BottomNavGraph.kt
@@ -0,0 +1,9 @@
+package com.example.bottomnavbardemo.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun BottomNavGraph(navController: NavHostController) {
+
+}
