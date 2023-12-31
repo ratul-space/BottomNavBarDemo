912bfc5128c8817a8edee3e975a062ba0fe303ba
ratul-space <to.ratul.hosen@gmail.com>
2023-12-31 23:26:22 +0600

    feat(MainScreen): add BottomNavGraph under Scaffold

--- app/src/main/java/com/example/bottomnavbardemo/MainScreen.kt
+++ app/src/main/java/com/example/bottomnavbardemo/MainScreen.kt
@@ -1,8 +1,20 @@
 package com.example.bottomnavbardemo
 
+import android.annotation.SuppressLint
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+import com.example.bottomnavbardemo.navigation.BottomNavGraph
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MainScreen() {
+    val navController = rememberNavController()
+
+    Scaffold(
+        bottomBar = {}
+    ) {
+        BottomNavGraph(navController = navController)
+    }
 
 }
