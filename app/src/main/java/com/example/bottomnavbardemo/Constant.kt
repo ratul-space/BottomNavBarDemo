f651e841b5a9767db56731e1e0c0b05a1a2b97fd
ratul-space <to.ratul.hosen@gmail.com>
2023-12-31 22:35:20 +0600

    feat: add route variables on Constant object

---
+++ app/src/main/java/com/example/bottomnavbardemo/Constant.kt
@@ -0,0 +1,7 @@
+package com.example.bottomnavbardemo
+
+object Constant{
+    const val NAV_ROUTE_HOME: String = "home"
+    const val NAV_ROUTE_PROFILE: String = "profile"
+    const val NAV_ROUTE_SETTINGS: String = "settings"
+}
