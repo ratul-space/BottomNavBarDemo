81e91f07f763f6e6416e757c28a1dcf1b43dcadb
ratul-space <to.ratul.hosen@gmail.com>
2023-12-31 22:39:31 +0600

    refactor: rename Constant to Route object

--- app/src/main/java/com/example/bottomnavbardemo/Route.kt
+++ app/src/main/java/com/example/bottomnavbardemo/Route.kt
@@ -1,6 +1,6 @@
 package com.example.bottomnavbardemo
 
-object Constant{
+object Route{
     const val NAV_ROUTE_HOME: String = "home"
     const val NAV_ROUTE_PROFILE: String = "profile"
     const val NAV_ROUTE_SETTINGS: String = "settings"
