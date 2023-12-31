68caa5d9116cbcdc990c648008586e0981b7d8e1
ratul-space <to.ratul.hosen@gmail.com>
2023-12-31 23:17:33 +0600

    feat(navigation): add 2 new composable ProfileScreen and SettingsScreen composable under NavHost

--- app/src/main/java/com/example/bottomnavbardemo/navigation/BottomNavGraph.kt
+++ app/src/main/java/com/example/bottomnavbardemo/navigation/BottomNavGraph.kt
@@ -5,6 +5,8 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.bottomnavbardemo.screens.HomeScreen
+import com.example.bottomnavbardemo.screens.ProfileScreen
+import com.example.bottomnavbardemo.screens.SettingsScreen
 
 @Composable
 fun BottomNavGraph(navController: NavHostController) {
@@ -14,6 +16,12 @@ fun BottomNavGraph(navController: NavHostController) {
     ) {
         composable(route = BottomBarScreen.Home.route) {
             HomeScreen()
+        }
+        composable(route = BottomBarScreen.Profile.route) {
+            ProfileScreen()
+        }
+        composable(route = BottomBarScreen.Settings.route) {
+            SettingsScreen()
         }
 
     }
